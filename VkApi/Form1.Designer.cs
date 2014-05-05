cf8854843566cb9b573536970620aed4d2648389
MaxNagibator <maxnagibator@gmail.com>
2014-05-05 19:48:40 +0700

    small change

--- VkApi/Form1.Designer.cs
+++ VkApi/Form1.Designer.cs
@@ -49,7 +49,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "13459966";
+            this.textBox1.Text = "68372827";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
@@ -58,7 +58,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "134384473";
+            this.textBox2.Text = "wall";
             // 
             // Form1
             // 
