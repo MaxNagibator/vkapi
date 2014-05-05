f20059f6ac927430f934dc1c36c72f747ecf089c
maxnagibator <managibator@gmail.com>
2014-05-05 18:55:02 +0700

    begin

---
+++ VkApi/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VkApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string sURL = "https://api.vk.com/method/photos.get?owner_id=" + textBox1.Text + "&album_id=wall";
+            WebRequest wrGETURL = WebRequest.Create(sURL);
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+            string sLine = "";
+            int i = 0;
+            StreamReader objReader = new StreamReader(objStream);
+            string a = "";
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                var text = String.Format("{0}:{1}", i, sLine);
+                if (sLine != null)
+                    //MessageBox.Show(text);
+                    a = sLine;
+            }
+            RootObject obj = Activator.CreateInstance<RootObject>();
+            MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(a));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            obj = (RootObject)serializer.ReadObject(ms);
+            WebClient webClient = new WebClient();
+            foreach (var vkImage in obj.response)
+            {
+                webClient.DownloadFile(vkImage.src_big, "D:\\" + vkImage.pid+".jpg");
+            }
+            ms.Close();
+        }
+    }
+}
---
+++ VkApi/VkImage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public class Response
+{
+    public int pid { get; set; }
+    public int aid { get; set; }
+    public int owner_id { get; set; }
+    public string src { get; set; }
+    public string src_big { get; set; }
+    public string src_small { get; set; }
+    public int width { get; set; }
+    public int height { get; set; }
+    public string text { get; set; }
+    public int created { get; set; }
+    public int post_id { get; set; }
+    public string src_xbig { get; set; }
+    public string src_xxbig { get; set; }
+    public string src_xxxbig { get; set; }
+}
+
+public class RootObject
+{
+    public List<Response> response { get; set; }
+}
